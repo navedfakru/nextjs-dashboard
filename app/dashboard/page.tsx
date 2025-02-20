6ef680f5a0a9efd0486c826ff7e12804f5aa7053
Naved <naved10055@gmail.com>
2025-02-20 20:30:17 +0530

    chapter 6

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <p className="sm:text-3xl text-sm text-center">Dashboard Page</p>;
+}
---
+++ app/naved/page.tsx
@@ -0,0 +1,5 @@
+export default function Naved() {
+  return (
+    <h1 className="text-xl font-bold font-sans">Nextjs with Naved Ali</h1>
+  )
+}
